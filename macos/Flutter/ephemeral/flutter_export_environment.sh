1e06f5ad95ecada6f138bdaca359dbf26df7d054
SamieJohnz <rodrielnt@gmail.com>
2022-12-22 20:33:31 -0800

    with mac files

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/SamieJohnz/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/SamieJohnz/StudioProjects/celebrated"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/SamieJohnz/StudioProjects/celebrated/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.1.1"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/SamieJohnz/StudioProjects/celebrated/.dart_tool/package_config.json"
